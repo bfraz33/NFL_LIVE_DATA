444b9494356a865c69a27693850dfcbc9a7aac94
Brandon Frazier <bfrazier756@gmail.com>
2025-09-03 20:37:55 -0500

    updating

--- publisher.py
+++ publisher.py
@@ -25,7 +25,7 @@ mqtt_connection.connect().result()
 print("✅ Connected!")
 
 # Publish a test message
-message = {"msg": "Successful try!!"}
+message = {"msg": "Successful attempt!"}
 mqtt_connection.publish(
     topic=TOPIC,
     payload=json.dumps(message),
